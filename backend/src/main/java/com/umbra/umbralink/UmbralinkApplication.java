4c65e69c5dab0f51d6292ffb1c8104b408ae9a0c
GA3RlEL <kruszkagabrys@gmail.com>
2025-01-04 16:06:14 +0100

    first commit

---
+++ backend/src/main/java/com/umbra/umbralink/UmbralinkApplication.java
@@ -0,0 +1,13 @@
+package com.umbra.umbralink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmbralinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UmbralinkApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/umbra/umbralink/UmbralinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umbra.umbralink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UmbralinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
