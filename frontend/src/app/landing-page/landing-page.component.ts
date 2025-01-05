9debcd77b2b096a8e9d80710603be31fbf3f2e3b
GA3RlEL <kruszkagabrys@gmail.com>
2025-01-06 00:39:28 +0100

    rebuilded code to handle routes and added login page with login component
    registration compoment TBD

--- frontend/src/app/landing-page/landing-page.component.ts
+++ frontend/src/app/landing-page/landing-page.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HeroSectionComponent } from "./hero-section/hero-section.component";
 
 @Component({
   selector: 'app-landing-page',
-  imports: [NavBarComponent, HeroSectionComponent],
+  imports: [HeroSectionComponent],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css'
 })
