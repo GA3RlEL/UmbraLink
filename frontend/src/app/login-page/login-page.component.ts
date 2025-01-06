ed1b820f81d8946116c1d83faef95427d3c2bc04
GA3RlEL <kruszkagabrys@gmail.com>
2025-01-06 14:10:11 +0100

    register component in login section

--- frontend/src/app/login-page/login-page.component.ts
+++ frontend/src/app/login-page/login-page.component.ts
@@ -1,11 +1,40 @@
+import { CssSelector } from '@angular/compiler';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login-page.component.html',
-  styleUrl: './login-page.component.css'
+  styleUrl: './login-page.component.css',
 })
 export class LoginPageComponent {
+  isLoginForm = true;
+  loginFrom = new FormGroup({
+    email: new FormControl('', [Validators.email, Validators.required]),
+    password: new FormControl('', [Validators.required])
+  })
+  registerForm = new FormGroup({
+    userName: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.email, Validators.required]),
+    password: new FormControl('', [Validators.required]),
+  })
+
+
+  changeForm() {
+    this.isLoginForm = !this.isLoginForm
+    this.loginFrom.reset();
+    this.registerForm.reset();
+  }
+
+  submitLoginForm() {
+    console.log(this.loginFrom.value);
+    console.log(this.loginFrom.errors);
+    console.log(this.loginFrom);
+  }
+
+  submitRegisterForm() {
+    console.log(this.registerForm.value);
+  }
 
 }
